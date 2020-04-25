198ab9a907611955198f16c81293c3c38ebd9cbc
vinayakdhingra19 <60654040+vinayakdhingra19@users.noreply.github.com>
2020-04-25 15:56:36 +0530

    Add files via upload

---
+++ js/Form.js
@@ -0,0 +1,25 @@
+class Form{
+    constructor (){
+
+    }
+    display(){
+        var title = createElement('h2');
+        title.html("CAR RACING GAME");
+        title.position(300,0);
+        var input = createInput("name");
+        var button = createButton('play');
+        var greeting = createElement('h3');
+        input.position(130,160);
+        button.position(250,200);
+        button.mousePressed(function(){
+            input.hide();
+            button.hide();
+            var name = input.value();
+            playerCount = playerCount + 1;
+            player.update(name);
+            player.updateCount(playerCount);
+            greeting.html("hello" + name );
+            greeting.position(130,160);
+        });
+    }
+}
